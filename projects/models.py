1f085533c3a5dde542e3f52047e2e58b8efca1bb
Daniel Barreto <125984531+danielbarretoes@users.noreply.github.com>
2024-01-19 16:15:05 +0100

    Model Update

--- projects/models.py
+++ projects/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Class Project permite crear una tabla en la base de datos conectada a Django.
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    tegnology = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
